ffc567dd9302d8cc2f9afa9d57d96d64e24a678e
davidecarizzoni <davide.carizzoni@runelab.it>
2024-04-27 16:26:34 +0200

    feat(init)

---
+++ app/meals/share/page.tsx
@@ -0,0 +1,9 @@
+export default function MealsSharePage() {
+	return (
+		<main>
+			<h1 style={{ color: 'white', textAlign: 'center' }}>
+				Meals Share - Time to get started!
+			</h1>
+		</main>
+	);
+}
