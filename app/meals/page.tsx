2a40d408cc65b9ffad8022b7288cf7c2154c4604
davidecarizzoni <davide.carizzoni@runelab.it>
2024-04-27 19:14:57 +0200

    feat(meals)

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,9 +1,26 @@
+import classes from "./page.module.scss";
+import MealsGrid from "@/components/meals-grid/meals-grid";
+import Link from "next/link";
+
 export default function MealsPage() {
 	return (
-		<main>
-			<h1 style={{ color: 'white', textAlign: 'center' }}>
-				Meals  - Time to get started!
-			</h1>
-		</main>
+		<>
+			<header className={classes.header}>
+				<h1>
+					Delicious meals, created <span className={classes.highlight}> by you </span>
+				</h1>
+				<p>
+					Choose your favorite meal and enjoy it
+				</p>
+				<p className={classes.cta}>
+					<Link href={'/meals/share'}>
+						Share your favorite receipe
+					</Link>
+				</p>
+			</header>
+			<main className={classes.main}>
+				<MealsGrid meals={[]}/>
+			</main>
+		</>
 	);
 }
--- components/meals-grid/meals-grid.tsx
+++ components/meals-grid/meals-grid.tsx
@@ -1,12 +1,14 @@
-import classes from "./meal-grid.module.scss";
+import classes from "./meals-grid.module.scss";
 import {Meal} from "@/types/meal";
-import MealItem from "@/components/meals/meal-item";
+import MealItem from "@/components/meal-item/meal-item";
 
 export default function MealsGrid ({meals}: {meals: Meal[]}) {
 	return (
 		<ul className={classes.meals}>
 			{meals.map(meal => (
-				<MealItem key={meal.id} {...meal} />
+				<li key={meal.id}>
+					<MealItem {...meal} />
+				</li>
 			))}
 		</ul>
 	)
